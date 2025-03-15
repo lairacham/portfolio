a17de7a341c8d7107800c26ed86d021a069879b7
lairacham <laira.cham@gmail.com>
2025-03-15 17:32:39 +0800

    feat(core): init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,27 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+  recommendedConfig: typescriptEslintPlugin.configs.recommended,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    plugins: ['@typescript-eslint', 'import', 'simple-import-sort'],
+    ignorePatterns: ['node_modules/', 'dist/', '.next/', 'pnpm-lock.yaml'],
+    rules: {
+      semi: ['error', 'never'],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig
