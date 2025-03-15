cc0ed44e785a37d91417451eba75bf92bdae3141
lairacham <laira.cham@gmail.com>
2025-03-15 19:55:10 +0800

    feat(header): add styles to header

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,5 +1,54 @@
+'use client'
+
+import { Menu, X } from 'lucide-react'
+import Link from 'next/link'
+import { useState } from 'react'
+
+import { useIsMobile } from '@/app/hooks/useIsMobile'
+
+import styles from './header.module.scss'
+
 const Header = () => {
-  return <div>Header</div>
+  const isMobile = useIsMobile()
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
+  return (
+    <div className={styles.headerContainer}>
+      <Link href='/'>Home</Link>
+      {isMobile ? (
+        <>
+          <Menu
+            className={isMenuOpen ? styles.displayNone : styles.icon}
+            onClick={handleMenuClick}
+          />
+          <div
+            className={
+              isMenuOpen ? styles.mobileHeaderItems : styles.displayNone
+            }
+          >
+            <Link href='/about'>About</Link>
+            <Link href='/works'>Works</Link>
+            <Link href='/contact-us'>Contact Us</Link>
+          </div>
+          <X
+            className={isMenuOpen ? styles.icon : styles.displayNone}
+            onClick={handleMenuClick}
+          />
+        </>
+      ) : (
+        <div className={styles.headerItems}>
+          <Link href='/about'>About</Link>
+          <Link href='/works'>Works</Link>
+          <Link href='/contact-us'>Contact Us</Link>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Header
